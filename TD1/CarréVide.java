5ec9e0a35df419f10c58ba3d48550a34bb3c34f0
gregory798 <73400124+gregory798@users.noreply.github.com>
2020-12-10 20:41:35 +0100

    Create CarréVide.java

---
+++ TD1/CarréVide.java
@@ -0,0 +1,83 @@
+class CarreVide {
+    public static void main(String[] args) {
+        int n;
+        if (args.length == 1) {
+            n = Integer.parseInt(args[0]);
+
+
+            System.out.println("--------- With a for loop : ---------");
+
+            for (int i = 1; i <= n; i++) {
+                if (i == 1 || i == n){
+                    for (int j=0; j<n;j++){
+                        System.out.print("*");
+                    }
+                }else{
+                    for (int j = 1; j<= n; j++){
+                        if (j == 1 || j == n){
+                            System.out.print("*");
+                        }else{
+                            System.out.print(" ");
+                        }
+                    }
+                }
+                System.out.println();
+            }
+
+
+
+            System.out.println("\n \n --------- With a while loop : ---------");
+
+            int i = 1;
+            while (i <= n) {
+                if (i == 1 || i == n){
+                    int j = 0;
+                    while (j<n){
+                        System.out.print("*");
+                        j++;
+                    }
+                }else{
+                    int j = 1;
+                    while (j<= n){
+                        if (j == 1 || j == n){
+                            System.out.print("*");
+                        }else{
+                            System.out.print(" ");
+                        }
+                        j++;
+                    }
+                }
+                System.out.println();
+                i++;
+            }
+
+
+        }
+        else{
+                System.out.println("Please enter 1 argument");
+            }
+
+    }
+}
+/*
+--------- With a for loop : ---------
+******
+*    *
+*    *
+*    *
+*    *
+******
+
+
+ --------- With a while loop : ---------
+******
+*    *
+*    *
+*    *
+*    *
+******
+
+*/
+
+
+
