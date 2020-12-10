4922da80ca0a04bd0f14bb6369a150a3a66b4e87
gregory798 <73400124+gregory798@users.noreply.github.com>
2020-12-10 19:48:39 +0100

    Create LosangeEtoiles.java

---
+++ TD1/LosangeEtoiles.java
@@ -0,0 +1,95 @@
+class LosangeEtoiles {
+    public static void main(String[] args) {
+        int n;
+        if (args.length == 1) {
+            n = Integer.parseInt(args[0]);
+
+
+            System.out.println("--------- With a for loop : ---------");
+
+            for (int i = 1; i <= n ; i++){
+                for (int j = 1; j <= i; j++){
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
+
+            for (int i = 1; i <= n-1; i++){
+                for (int j = 1; j <= i; j++){
+                    System.out.print(" ");
+                }
+                for (int k = 3; k >= i; k--){
+                    System.out.print("*");
+
+                }
+                System.out.println();
+            }
+
+
+            System.out.println("\n \n --------- With a while loop : ---------");
+
+            int i = 1;
+            while (i <= n){
+                int j = 1;
+                while (j <= i){
+                    System.out.print("*");
+                    j++;
+                }
+
+                System.out.println();
+                i++;
+            }
+
+
+            int k = 1;
+            while (k <= n-1){
+                int l = 1;
+                while (l <= k){
+                    System.out.print(" ");
+                    l++;
+                }
+                int m = 3;
+                while (m >= k){
+                    System.out.print("*");
+                    m--;
+
+                }
+                System.out.println();
+                k++;
+            }
+
+
+        }
+
+
+        else{
+            System.out.println("Please enter only one argument");
+        }
+    }
+}
+/*
+
+--------- With a for loop : ---------
+*
+**
+***
+****
+ ***
+  **
+   *
+
+
+ --------- With a while loop : ---------
+*
+**
+***
+****
+ ***
+  **
+   *
+
+*/
+
+
+
